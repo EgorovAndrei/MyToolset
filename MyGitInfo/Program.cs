f4c8010d6ddb65ac59050cfb8710f0809cbde841
admin <anstan@inbox.ru>
2022-07-04 12:28:38 +0400

    MyGitInfo information about commit in assembly

---
+++ MyGitInfo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyGitInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(ThisAssembly.Git.Commit);
+        }
+    }
+}
