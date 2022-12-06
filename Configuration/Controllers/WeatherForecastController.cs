3b00fb982f9449188e779650bc56e6c35ad164f7
admin <anstan@inbox.ru>
2022-12-06 16:45:36 +0300

    Configuration

---
+++ Configuration/Controllers/WeatherForecastController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Configuration.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet(Name = "Test")]
+        public void Test()
+        {
+            var confParam = _configuration["TestParam"];
+        }
+    }
+}
---
+++ Configuration/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//builder.Configuration.AddJsonFile("appsettings.json")
+//    .AddJsonFile($"appsettings.{app.Environment.EnvironmentName}.json");
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    // пути для SetBasePath
+    var binPathLocal = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+    var binPathRelease = hostingContext.HostingEnvironment.ContentRootPath;
+
+    config
+    .SetBasePath(binPathLocal)
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json",true, true)
+    .AddEnvironmentVariables();
+
+    if(args != null)
+    {
+        config.AddCommandLine(args);
+    }
+});
+
+var app = builder.Build();
+
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
